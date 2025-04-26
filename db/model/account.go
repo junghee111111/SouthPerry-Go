5a722231e23c37babf8baf0cff8bb253d704394b
junghee111111 <jungheejjang1004@gmail.com>
2025-04-26 21:17:51 +0900

    Update Account Schema

--- db/model/account.go
+++ db/model/account.go
@@ -8,10 +8,19 @@ package model
 
 import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
 )
 
 type Account struct {
 	Id           primitive.ObjectID `bson:"_id,omitempty"`
 	Email        string             `bson:"email"`
 	PasswordHash string             `bson:"passwordHash"`
+	AccId        int32              `bson:"accId"`
+	IsGM         bool               `bson:"isGM"`
+	IsAdult      bool               `bson:"isAdult"`
+	Gender       int32              `bson:"gender"`
+
+	CreatedAt time.Time `bson:"createdAt"`
+	LoggedAt  time.Time `bson:"loggedAt"`
+	UpdatedAt time.Time `bson:"updatedAt"`
 }
